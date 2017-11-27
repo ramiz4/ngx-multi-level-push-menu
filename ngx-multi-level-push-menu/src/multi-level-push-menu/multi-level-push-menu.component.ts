5a7d76fe892e02d52bf03f251ded53c2baf75f69
ramiz4 <ramiz4@gmx.de>
2017-11-27 19:48:00 +0100

    refactoring

--- ngx-multi-level-push-menu/src/multi-level-push-menu/multi-level-push-menu.component.ts
+++ ngx-multi-level-push-menu/src/multi-level-push-menu/multi-level-push-menu.component.ts
@@ -8,6 +8,7 @@ import { MultiLevelPushMenuOptions, MultiLevelPushMenuItem } from './multi-level
 declare var $: any;
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'multi-level-push-menu',
   templateUrl: './multi-level-push-menu.component.html',
   styleUrls: ['./multi-level-push-menu.component.css']
@@ -24,17 +25,17 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
   collapseSubscription: Subscription;
   expandSubscription: Subscription;
 
-  constructor(private router: Router, private mlpmService: MultiLevelPushMenuService) {
-    this.initializeSubscription = this.mlpmService.initialized().subscribe(options => {
+  constructor(private router: Router, private multiLevelPushMenuService: MultiLevelPushMenuService) {
+    this.initializeSubscription = this.multiLevelPushMenuService.initialized().subscribe(options => {
       this.initMenu(options);
     });
-    this.updateSubscription = this.mlpmService.updated().subscribe(items => {
+    this.updateSubscription = this.multiLevelPushMenuService.updated().subscribe(items => {
       this.updateMenu(items);
     });
-    this.collapseSubscription = this.mlpmService.collapsed().subscribe(level => {
+    this.collapseSubscription = this.multiLevelPushMenuService.collapsed().subscribe(level => {
       this.collapseMenu(level);
     });
-    this.expandSubscription = this.mlpmService.expanded().subscribe(value => {
+    this.expandSubscription = this.multiLevelPushMenuService.expanded().subscribe(value => {
       this.expandMenu();
     });
   }
@@ -43,33 +44,33 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
     if (!options.menuWidth) {
       options.menuWidth = '300px';
     }
-    let offsetLeft = 'calc(' + options.menuWidth + ' + 20px)';
+    const offsetLeft = 'calc(' + options.menuWidth + ' + 20px)';
     $(this.elContent.nativeElement).css('left', offsetLeft);
     $(this.elContent.nativeElement).css('width', 'calc(100% - (' + options.menuWidth + ' + 20px))');
 
-    let router = this.router;
+    const router = this.router;
 
     $(this.elMenu.nativeElement).multilevelpushmenu({
-      container: $(this.elMenu.nativeElement),                 // Holding container.
-      containersToPush: [$(this.elContent.nativeElement)],     // Array of objects to push/slide together with menu.
-      collapsed: options.collapsed,                            // Initialize menu in collapsed/expanded mode
-      menuID: options.menuID,                                  // ID of <nav> element.
-      wrapperClass: options.wrapperClass,                      // Wrapper CSS class.
-      menuInactiveClass: options.menuInactiveClass,            // CSS class for inactive wrappers.
-      menu: [options.menu],                                    // JS array of menu items (if markup not provided).
-      menuWidth: options.menuWidth,                            // Wrapper width (integer, '%', 'px', 'em').
-      menuHeight: options.menuHeight,                          // Menu height (integer, '%', 'px', 'em').
-      backText: options.backText,                              // Text for 'Back' menu item.
-      backItemClass: options.backItemClass,                    // CSS class for back menu item.
-      backItemIcon: options.backItemIcon,                      // FontAvesome icon used for back menu item.
-      groupIcon: options.groupIcon,                            // FontAvesome icon used for menu items contaning sub-items.
-      mode: options.mode,                                      // Menu sliding mode: overlap/cover.
-      overlapWidth: options.overlapWidth,                      // Width in px of menu wrappers overlap
-      preventItemClick: options.preventItemClick,              // set to false if you do not need event callback functionality per item click
-      preventGroupItemClick: options.preventGroupItemClick,    // set to false if you do not need event callback functionality per group item click
-      direction: options.direction,                            // set to 'rtl' for reverse sliding direction
-      fullCollapse: options.fullCollapse,                      // set to true to fully hide base level holder when collapsed
-      swipe: options.swipe,                                    // or 'touchscreen', or 'desktop', or 'none'. everything else is concidered as 'none'
+      container: $(this.elMenu.nativeElement),                // Holding container.
+      containersToPush: [$(this.elContent.nativeElement)],    // Array of objects to push/slide together with menu.
+      collapsed: options.collapsed,                           // Initialize menu in collapsed/expanded mode
+      menuID: options.menuID,                                 // ID of <nav> element.
+      wrapperClass: options.wrapperClass,                     // Wrapper CSS class.
+      menuInactiveClass: options.menuInactiveClass,           // CSS class for inactive wrappers.
+      menu: [options.menu],                                   // JS array of menu items (if markup not provided).
+      menuWidth: options.menuWidth,                           // Wrapper width (integer, '%', 'px', 'em').
+      menuHeight: options.menuHeight,                         // Menu height (integer, '%', 'px', 'em').
+      backText: options.backText,                             // Text for 'Back' menu item.
+      backItemClass: options.backItemClass,                   // CSS class for back menu item.
+      backItemIcon: options.backItemIcon,                     // FontAwesome icon used for back menu item.
+      groupIcon: options.groupIcon,                           // FontAwesome icon used for menu items containing sub-items.
+      mode: options.mode,                                     // Menu sliding mode: overlap/cover.
+      overlapWidth: options.overlapWidth,                     // Width in px of menu wrappers overlap
+      preventItemClick: options.preventItemClick,             // set to false if you don't need event callback functionality per item click
+      preventGroupItemClick: options.preventGroupItemClick,   // set to false if you don't need event cb functionality per group item click
+      direction: options.direction,                           // set to 'rtl' for reverse sliding direction
+      fullCollapse: options.fullCollapse,                     // set to true to fully hide base level holder when collapsed
+      swipe: options.swipe,                                   // 'touchscreen', 'desktop' or 'none' everything else is considered as 'none'
 
       onItemClick: function () {
         const $item = arguments[2];
@@ -90,7 +91,7 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
   updateMenu(items: Array<MultiLevelPushMenuItem>): void {
     if (JSON.stringify(this.oldMenuItems.sort()) !== JSON.stringify(items.sort())) {
       // Add new items
-      var $addTo = $(this.elMenu.nativeElement).multilevelpushmenu('activemenu').first();
+      const $addTo = $(this.elMenu.nativeElement).multilevelpushmenu('activemenu').first();
       $(this.elMenu.nativeElement).multilevelpushmenu('additems', items, $addTo, 0);
       // Remove old items
       this.oldMenuItems.forEach(item => {
@@ -102,7 +103,7 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
   }
 
   collapseMenu(level: number) {
-    if(level) {
+    if (level) {
       // Collapse menu down to level 1 (0 level represent root level expanded)
       $(this.elMenu.nativeElement).multilevelpushmenu('collapse', level);
     } else {
@@ -113,7 +114,8 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
     // Collapse menu down to the level of $menuLevelObject
     // $('#menu').multilevelpushmenu('collapse', $menuLevelObject);
 
-    // Collapse menu down to the level of menu level object with title 'Devices' (not really recommended since there could be many menu level objects with the same title; in such cases collasing will be unsuccessful)
+    // Collapse menu down to the level of menu level object with title 'Devices' (not really recommended since there could be many menu 
+    // level objects with the same title; in such cases collapsing will be unsuccessful)
     // $('#menu').multilevelpushmenu('collapse', 'Devices');
   }
 
@@ -124,7 +126,8 @@ export class MultiLevelPushMenuComponent implements OnDestroy {
     // // Expand menu up to the $menuLevelObject
     // $('#menu').multilevelpushmenu('expand', $menuLevelObject);
 
-    // // Expand menu up to the menu level object with title 'Devices' (not recommended since there could be many menu level objects with the same title; in such cases expanding will be unsuccessful)
+    // Expand menu up to the menu level object with title 'Devices' (not recommended since there could be many menu level objects with the
+    // same title; in such cases expanding will be unsuccessful)
     // $('#menu').multilevelpushmenu('expand', 'Devices');
   }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { 
-  MultiLevelPushMenuService, 
-  MultiLevelPushMenu, 
-  MultiLevelPushMenuOptions, 
-  MultiLevelPushMenuItem 
+import {
+  MultiLevelPushMenuService,
+  MultiLevelPushMenu,
+  MultiLevelPushMenuOptions,
+  MultiLevelPushMenuItem
 } from '@ramiz4/ngx-multi-level-push-menu';
 
 @Component({
@@ -15,34 +15,33 @@ export class AppComponent implements OnInit {
 
   defaultItems: Array<MultiLevelPushMenuItem> = new Array<MultiLevelPushMenuItem>();
 
-  constructor(private mlpmService: MultiLevelPushMenuService) {}
+  constructor(private multiLevelPushMenuService: MultiLevelPushMenuService) {}
 
   ngOnInit() {
     this.defaultItems.push(new MultiLevelPushMenuItem('Home', 'home'));
     this.defaultItems.push(new MultiLevelPushMenuItem('About us', 'about-us'));
-    let options: MultiLevelPushMenuOptions = new MultiLevelPushMenuOptions();
+    const options: MultiLevelPushMenuOptions = new MultiLevelPushMenuOptions();
     options.mode = 'cover';
     options.menu = new MultiLevelPushMenu('Explorer', 'explorer', 'fa fa-reorder', this.defaultItems);
-    this.mlpmService.initialize(options);
+    this.multiLevelPushMenuService.initialize(options);
   }
 
   resetMenu(): void {
-    this.mlpmService.update(this.defaultItems);
+    this.multiLevelPushMenuService.update(this.defaultItems);
   }
 
   updateMenu(): void {
-    let newItems = new Array<MultiLevelPushMenuItem>();
+    const newItems = new Array<MultiLevelPushMenuItem>();
     newItems.push(new MultiLevelPushMenuItem('Collections', 'collections'));
     newItems.push(new MultiLevelPushMenuItem('Credits', 'credits'));
-    this.mlpmService.update(newItems);
+    this.multiLevelPushMenuService.update(newItems);
   }
 
   collapseMenu(): void {
-    this.mlpmService.collapse();
+    this.multiLevelPushMenuService.collapse(3);
   }
 
   expandMenu(): void {
-    this.mlpmService.expand();
+    this.multiLevelPushMenuService.expand();
   }
-
 }

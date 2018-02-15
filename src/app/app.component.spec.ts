58c0b29c06cb1f604266cc2b71c8b49da13d2d46
ramiz4 <ramiz4@gmx.de>
2018-02-15 20:46:05 +0100

    fixed unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterModule, Routes } from '@angular/router';
 import { MultiLevelPushMenuModule } from '@ramiz4/ngx-multi-level-push-menu';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { CreditsComponent } from './credits/credits.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'credits', component: CreditsComponent },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterModule.forRoot(routes),
         MultiLevelPushMenuModule.forRoot()
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        AboutUsComponent,
+        CollectionsComponent,
+        CreditsComponent,
+        PageNotFoundComponent
       ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
     }).compileComponents();
   }));
 
